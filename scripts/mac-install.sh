8509ca26a9198ca2cf2444ab0114cf59ea1d1e91
Jakob Homan <jghoman@gmail.com>
2015-10-05 16:36:23 -0700

    bash, mac install py27-pip

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -2,7 +2,7 @@
 set -e # Bail early
 #set -x # Very verbose
 
-sudo port install tmux vim git llvm-3.8 htop glances mutt bash  clang-3.4 the_silver_searcher maven3 tree ncdu
+sudo port install tmux vim git llvm-3.8 htop glances mutt bash  clang-3.4 the_silver_searcher maven3 tree ncdu py27-pip
 
 sudo port select --set maven maven3
-
+sudo port select --set pip pip27
