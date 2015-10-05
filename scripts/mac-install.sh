ab4ffa0a7be0c7d745cec1cd93e37f327d73c06f
Jakob Homan <jghoman@gmail.com>
2015-10-05 10:34:50 -0700

    add silver surfer to mac install.

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -2,4 +2,4 @@
 set -e # Bail early
 #set -x # Very verbose
 
-sudo port install tmux vim git llvm-3.8 htop glances mutt bash  clang
+sudo port install tmux vim git llvm-3.8 htop glances mutt bash  clang the_silver_searcher
