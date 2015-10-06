2d935e9bfa7a9a9a85db69815c35007278d12e30
Jakob Homan <jghoman@gmail.com>
2015-10-06 14:50:15 -0700

    bash, mac setup, add jq

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -2,7 +2,22 @@
 set -e # Bail early
 #set -x # Very verbose
 
-sudo port install tmux vim git llvm-3.8 htop glances mutt bash  clang-3.4 the_silver_searcher maven3 tree ncdu py27-pip
+sudo port install \
+    tmux \
+    vim \
+    git \
+    llvm-3.8 \
+    htop \
+    glances \
+    mutt \
+    bash \
+    clang-3.4 \
+    the_silver_searcher \
+    maven3 \
+    tree \
+    ncdu \
+    py27-pip \
+    jq
 
 sudo port select --set maven maven3
 sudo port select --set pip pip27
