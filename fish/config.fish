707c6c7fb4241feb1d2c6f6e7da2f70b8c57d95c
Jakob Homan <jghoman@gmail.com>
2016-05-07 20:45:27 -0700

    Fish: Beginning of config.fish

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+# random time savers
+alias fucking "sudo"
+alias gcm "git checkout master"
+alias gco "git checkout"
+alias gp "git pull"
+alias gl "git lg"
+alias gca "git commit -a"
+alias gpom "git push origin master"
+alias gcdf "git clean -d -f"
+alias gd "git diff --no-prefix"
+
+alias gti git
+
+
