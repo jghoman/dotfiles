a88d9801c81ac73407ecac8cbb6d2a7438a70c77
Jakob Homan <jghoman@gmail.com>
2016-05-07 21:19:56 -0700

    fish: Add git prompt configuration

--- fish/config.fish
+++ fish/config.fish
@@ -11,4 +11,31 @@ alias gd "git diff --no-prefix"
 
 alias gti git
 
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
 
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'verbose'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_show_informative_status 'yes'
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
+end
