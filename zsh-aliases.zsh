822a886fa1078f2ca0b03ece1e76aa38d46db83b
Jakob Homan <jghoman@gmail.com>
2025-07-31 22:58:29 -0700

    Add some more aliases.

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -1,6 +1,11 @@
 alias spell="just --justfile=justfile"
 alias b="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
 
+alias c="clear"
+
+alias e="eza"
+alias ls="eza"
+
 alias gco="git checkout"
 alias gc="git commit"
 alias gb="git checkout -b"
