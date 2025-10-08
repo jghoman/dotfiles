f4c1ad5dc7f9d3d7ab7f11c65a78af29e4e1e2ee
Jakob Homan <jghoman@gmail.com>
2025-10-08 11:58:35 -0700

    Update aliases.

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -1,11 +1,12 @@
-alias spell="just --justfile=justfile"
 alias b="bat --theme=\$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
+alias spell="just --justfile=justfile"
 
+alias br="broot"
 alias c="clear"
-
 alias e="eza"
+alias l="eza"
 alias ls="eza"
 
-alias gco="git checkout"
-alias gc="git commit"
 alias gb="git checkout -b"
+alias gc="git commit"
+alias gco="git checkout"
