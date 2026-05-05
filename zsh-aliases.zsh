e50f783fefed5992e292c15968b0ca0a038cecf7
Jakob Homan <jghoman@gmail.com>
2026-05-05 16:35:16 -0700

    tmux updates.

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -15,6 +15,19 @@ alias j="just"
 alias jx="uvx justx"
 alias calcure="uvx calcure"
 
+inpane() {
+  local chan="inpane-$$-$RANDOM"
+  local current_pane=$(tmux display-message -p '#{pane_id}')
+  local pane_count=$(tmux list-panes | wc -l | tr -d ' ')
+  if [ "$pane_count" -eq 1 ]; then
+    tmux split-window -h "$*; tmux wait-for -S $chan"
+  else
+    tmux split-window -v -t '{right}' "$*; tmux wait-for -S $chan"
+  fi
+  tmux select-pane -t "$current_pane"
+  { tmux wait-for $chan; sleep 0.1; tmux select-pane -t "$current_pane"; } &!
+}
+
 newsrc() {
   if tmux list-windows -F "#{window_name}" | grep -qx "$1"; then
     tmux select-window -t "=$1"
